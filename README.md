cce5e554ea111014fbf53d969ec32e57db91353f
sbachmei <sbachmei@uw.edu>
2023-08-10 12:49:16 -0600

    save here

--- README.md
+++ README.md
@@ -21,12 +21,18 @@ entity resolution (ER) pipelines.
 NOTE: Each pipeline step gets its own sub-folder in the "linker/" parent
 directory, e.g. "linker/pvs_like_case_study_sample_data/".
 
+If the image file(s) (.tar) have not been provided, you can create them from the
+Dockerfile; refer to the `Creating a docker image to be shared` section, below.
+
+Assuming you have the image .tar file, you can run the pipeline by:
+
 ```
-$ sh <path-to-repo>/run_step.sh <step>
-$ # e.g. from linker/, `sh run_step.sh pvs_like_case_study_sample_data`
+$ sh <path-to-repo>/run_step.sh <STEP-FOLDER> <CONTAINER-PLATFORM>
+$ # e.g. from linker/, `sh run_step.sh pvs_like_case_study_sample_data docker`
 ```
 
-This will build the image from the Dockerfile, run the step in the container, and save the output file in the results folder.
+This will spin up a container from the image (converting to singularity if
+necessary) and run it.
 
 ### Requirements
 
@@ -42,11 +48,11 @@ To create an image .tar file from a Dockerfile,  first ensure Docker is installe
 ```
 $ cd linker/pvs_like_case_study_sample_data/
 $ docker build -t linker:pvs_like_case_study_sample_data .
-$ # Convert to .tar.gz
+$ # Convert to tar.gz file with gzip
 $ docker save linker | gzip > image.tar.gz
 $ # Remove the image
 $ docker rmi linker
 ```
 
-You should now have a step.tar image file alongside the Dockerfile which can be
-used to spin up the container.
+You should now have a docker image.tar.gz image file alongside the Dockerfile
+which can be used to spin up the container.
--- run_step.sh
+++ run_step.sh
@@ -2,39 +2,75 @@
 
 set -e
 
-usage_comment="Usage: $0 <step path>";
+USAGE_COMMENT="
+*** ERROR ***
+Usage: $0 <STEP-FOLDER> <CONTAINER-PLATFORM>
+Arguments:
+    STEP-FOLDER - Name of step-specific sub-directory
+    CONTAINER-PLATFORM - 'docker' or 'singularity'
+"
 
-# TODO: support a "singularity" or "docker" input arg
 # TODO: support user-provided input files
 # e.g. docker cp some-directory/* $image_id:/app/ 
 
-if [[ -z "$1" ]]; then
-    echo $usage_comment
-    echo "Please provide the step-specific directory."
+if [ "$#" -lt 2 ]; then
+    echo "$USAGE_COMMENT"
+    echo "Please provide all required arguments."
     exit 1
 fi
 
-dir=$(realpath "$1");
-step=$(basename $dir);
+DIR=$(realpath "$1")
+PLATFORM="$2"
+
+echo "$DIR"
+if [ ! -d "$DIR" ]; then
+    echo "$USAGE_COMMENT"
+    echo "STEP-FOLDER provided ('$DIR') does not exist."
+    exit 1
+fi
+
+if [ "$PLATFORM" != "docker" ] && [ "$PLATFORM" != "singularity" ]; then
+    echo "$USAGE_COMMENT"
+    echo "CONTAINER-PLATFORM  must be 'docker' or 'singularity'; provided '$PLATFORM'."
+    exit 1
+fi
+
+STEP=$(basename $DIR)
 
 # TODO: implement versioning
-mkdir $dir/results
+mkdir $DIR/results
+
+if [ "$PLATFORM" = "docker" ]; then
+    echo ""
+    echo "Loading the image"
+    # TODO: make flexibile to different compressions (bzip2 or xz) or no compression
+    docker load -i $DIR/image.tar.gz 2>&1 | tee $DIR/results/docker.o
+    IMAGE_ID=$(docker images --filter=reference=linker:$STEP --format "{{.ID}}")
+    echo "Loaded image with image ID $IMAGE_ID"
+
+    echo ""
+    echo "Running the image"
+    docker run --rm -v $DIR/input_data:/app/input_data/ -v $DIR/results:/app/results/ $IMAGE_ID 2>&1 | tee -a $DIR/results/docker.o
+
+    echo ""
+    echo "Removing the image"
+    docker rmi $IMAGE_ID
+fi
+
+if [ "$PLATFORM" = "singularity" ]; then
+    echo ""
+    echo "Converting the docker image to singularity"
+    singularity build $DIR/image.sif docker-archive://$DIR/image.tar.gz 2>&1 | tee $DIR/results/singularity.o
+
+    echo ""
+    echo "Running the image"
+    singularity run $DIR/image.sif 2>&1 | tee -a $DIR/results/singularity.o
+
+    echo ""
+    echo "Moving image file into results"
+    mv $DIR/image.sif $DIR/results
+fi
 
 echo ""
-echo "Loading the image"
-# TODO: make flexible to different compressions (bzip2 or xz) or no compression
-docker load -i $dir/image.tar.gz 2>&1 | tee $dir/results/docker.o
-image_id=$(docker images --filter=reference=linker:$step --format "{{.ID}}")
-echo "Loaded image with image ID $image_id"
-
-echo ""
-echo "Running the step"
-docker run --rm -v $dir/input_data:/app/input_data/ -v $dir/results:/app/results/ $image_id 2>&1 | tee -a $dir/results/docker.o
-
-echo ""
-echo "Removing image"
-docker rmi $image_id
-
-echo ""
-echo "*** finished ***"
+echo "*** finished running the step ($PLATFORM) ***"
 echo ""
