d90083634650bc24511df3382adc2c2f98bce197
sbachmei <sbachmei@uw.edu>
2023-08-15 11:46:43 -0600

    change license to bsd-3-clause

--- src/linker/__about__.py
+++ src/linker/__about__.py
@@ -18,5 +18,5 @@ __version__ = "0.0.1"
 __author__ = "The vivarium developers"
 __email__ = "vivarium.dev@gmail.com"
 
-__license__ = "GNU GPLv3"
+__license__ = "BSD-3-Clause"
 __copyright__ = f"Copyright 2023 {__author__}"
