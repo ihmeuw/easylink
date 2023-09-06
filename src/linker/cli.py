362f28e485808e288b2cbfd049a231a7637f9f15
Steve Bachmeier <sbachmei@uw.edu>
2023-09-06 11:41:16 -0700

    major refactor

--- src/linker/cli.py
+++ src/linker/cli.py
@@ -71,8 +71,8 @@ def run(
     """
     configure_logging_to_terminal(verbose)
     config = Config(
-        pipeline_path=pipeline_specification,
-        computing_environment_input=computing_environment,
+        pipeline_specification=pipeline_specification,
+        computing_environment=computing_environment,
     )
     # TODO [MIC-4493]: Add configuration validation
     results_dir = prepare_results_directory(config)
@@ -89,10 +89,6 @@ def run(
 
 @linker.command()
 @click.argument(
-    "pipeline_specification",
-    type=click.Path(exists=True, dir_okay=False, resolve_path=True),
-)
-@click.argument(
     "container_engine",
     type=click.Choice(["docker", "singularity", "unknown"]),
 )
@@ -100,21 +96,33 @@ def run(
     "results_dir",
     type=click.Path(exists=True, resolve_path=True),
 )
+@click.argument("step_name")
+@click.argument(
+    "step_dir",
+    type=click.Path(exists=True, resolve_path=True),
+)
 @click.option("-v", "verbose", count=True, help="Configure logging verbosity.", hidden=True)
 def run_slurm_job(
-    pipeline_specification: str, container_engine: str, results_dir: str, verbose: int
+    container_engine: str,
+    results_dir: str,
+    step_name: str,
+    step_dir: str,
+    verbose: int,
 ) -> None:
     """(TEMPORARY COMMAND FOR DEVELOPMENT) Runs a job on Slurm. The standard use case is this would be kicked off
     when a slurm computing environment is defined in the environment.yaml
     """
     configure_logging_to_terminal(verbose)
-    config = Config(pipeline_path=pipeline_specification, computing_environment_input="local")
     results_dir = Path(results_dir)
-    main = handle_exceptions(func=runner.main, exceptions_logger=logger, with_debugger=False)
+    step_dir = Path(step_dir)
+    main = handle_exceptions(
+        func=runner.run_container, exceptions_logger=logger, with_debugger=False
+    )
     main(
-        config=config,
         container_engine=container_engine,
         results_dir=results_dir,
+        step_name=step_name,
+        step_dir=step_dir,
     )
     # TODO: Update log message to be more clear when job is launched
     #   (i.e. "finished" is not a good message when the job is still running)
