a3797ab06b5e64565db47f40193ccc927866d0b4
Steve Bachmeier <sbachmei@uw.edu>
2023-09-08 14:02:21 -0700

    fix breaking pytests

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -19,18 +19,29 @@ PIPELINE_CONFIG_DICT = {
 
 
 @pytest.fixture(scope="session")
-def env_config_path(tmpdir_factory):
-    tmp_path = str(tmpdir_factory.getbasetemp())
-    filepath = f"{tmp_path}/environment.yaml"
-    with open(filepath, "w") as file:
-        yaml.dump(ENV_CONFIG_DICT, file)
-    return filepath
+def config_path(tmpdir_factory):
+    tmp_path = tmpdir_factory.getbasetemp()
+    # pipeline
+    with open(f"{str(tmp_path)}/pipeline.yaml", "w") as file:
+        yaml.dump(PIPELINE_CONFIG_DICT, file, sort_keys=False)
+    # environment
+    with open(f"{str(tmp_path)}/environment.yaml", "w") as file:
+        yaml.dump(ENV_CONFIG_DICT, file, sort_keys=False)
+    # input
+    input_dir1 = tmp_path.mkdir("input_data1")
+    input_dir2 = tmp_path.mkdir("input_data2")
+    with open(f"{str(input_dir1)}/file1", "w") as file:
+        file.write("")
+    with open(f"{str(input_dir2)}/file2", "w") as file:
+        file.write("")
+    with open(f"{tmp_path}/input_data.yaml", "w") as file:
+        yaml.dump(
+            {
+                "foo": str(input_dir1 / "file1"),
+                "bar": str(input_dir2 / "file2"),
+            },
+            file,
+            sort_keys=False,
+        )
 
-
-@pytest.fixture(scope="session")
-def pipeline_config_path(tmpdir_factory):
-    tmp_path = str(tmpdir_factory.getbasetemp())
-    filepath = f"{tmp_path}/pipeline.yaml"
-    with open(filepath, "w") as file:
-        yaml.dump(PIPELINE_CONFIG_DICT, file)
-    return filepath
+    return str(tmp_path)
