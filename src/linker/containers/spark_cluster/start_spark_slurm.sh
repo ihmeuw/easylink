da27b429d9e65ea902068633bb55d37cf8d2a1c4
Matthew Kappel <mkappel@uw.edu>
2023-11-02 14:27:08 -0700

    launcher

--- src/linker/containers/spark_cluster/start_spark_slurm.sh
+++ src/linker/containers/spark_cluster/start_spark_slurm.sh
@@ -22,7 +22,6 @@ export SPARK_LOCAL_DIRS=$HOME/.spark_temp/logs
 export SPARK_MASTER_PORT=28508
 export SPARK_MASTER_WEBUI_PORT=28509
 export SPARK_WORKER_CORES=$SLURM_CPUS_PER_TASK
-# shellcheck disable=SC2004
 export SPARK_DAEMON_MEMORY=$(( $SLURM_MEM_PER_CPU * $SLURM_CPUS_PER_TASK / 2 ))m
 export SPARK_MEM=$SPARK_DAEMON_MEMORY
 
@@ -43,13 +42,19 @@ else
         MASTER_NODE=$( scontrol show hostname "$SLURM_NODELIST "| head -n 1 )
 
         mkdir -p "/tmp/pvs_like_case_study_spark_local_$USER"
-        singularity exec -B /mnt:/mnt,"/tmp/pvs_like_case_study_spark_local_$USER":/tmp $SINGULARITY_IMG $CONDA_PATH run --no-capture-output -n $CONDA_ENV "$SPARK_ROOT/bin/spark-class" org.apache.spark.deploy.master.Master --host "$SPARK_MASTER_IP" --port "$SPARK_MASTER_PORT" --webui-port "$SPARK_MASTER_WEBUI_PORT"
+        singularity exec -B /mnt:/mnt,"/tmp/pvs_like_case_study_spark_local_$USER":/tmp $SINGULARITY_IMG \
+         $CONDA_PATH run --no-capture-output -n $CONDA_ENV "$SPARK_ROOT/bin/spark-class" \
+         org.apache.spark.deploy.master.Master --host "$SPARK_MASTER_IP" --port "$SPARK_MASTER_PORT" \
+         --webui-port "$SPARK_MASTER_WEBUI_PORT"
     else
-        # TODO: This step assumes that SLURM_PROCID=0 corresponds to the first node in SLURM_NODELIST. Is this reasonable?
+        # TODO: This step assumes that SLURM_PROCID=0 corresponds to the first node in SLURM_NODELIST.
+        #  Is this reasonable?
         MASTER_NODE=spark://$( scontrol show hostname "$SLURM_NODELIST" | head -n 1 ):"$SPARK_MASTER_PORT"
 
         mkdir -p "/tmp/pvs_like_case_study_spark_local_$USER"
-        singularity exec -B /mnt:/mnt,"/tmp/pvs_like_case_study_spark_local_$USER":/tmp "$SINGULARITY_IMG" "$CONDA_PATH" run --no-capture-output -n "$CONDA_ENV" "$SPARK_ROOT/bin/spark-class" org.apache.spark.deploy.worker.Worker "$MASTER_NODE"
+        singularity exec -B /mnt:/mnt,"/tmp/pvs_like_case_study_spark_local_$USER":/tmp "$SINGULARITY_IMG" \
+        "$CONDA_PATH" run --no-capture-output -n "$CONDA_ENV" "$SPARK_ROOT/bin/spark-class" \
+         org.apache.spark.deploy.worker.Worker "$MASTER_NODE"
     fi
 fi
 
