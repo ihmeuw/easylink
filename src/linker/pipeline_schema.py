82e8b6135ce4df0401fd2d8e67b3ec2b3108cfeb
sbachmei <sbachmei@uw.edu>
2023-12-11 12:49:06 -0700

    refactor into a _generate_schema class method

--- src/linker/pipeline_schema.py
+++ src/linker/pipeline_schema.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 from linker.step import Step
 
@@ -21,20 +21,30 @@ class PipelineSchema:
         schemas = []
 
         # pvs-like case study
-        pvs_like_case_study = cls("pvs_like_case_study")
-        pvs_like_case_study._add_step(Step("pvs_like_case_study"))
-        schemas.append(pvs_like_case_study)
+        schemas.append(PipelineSchema._generate_schema(
+            "pvs_like_case_study",
+            Step("pvs_like_case_study"),
+            )
+        )
 
         # development dummy
-        development = cls("development")
-        development._add_step(Step("step_1"))
-        development._add_step(Step("step_2"))
-        schemas.append(development)
-
+        schemas.append(PipelineSchema._generate_schema(
+            "development",
+            Step("step_1"),
+            Step("step_2"),
+            )
+        )
+        
         return schemas
 
     def _add_step(self, step: Step) -> None:
         self.steps.append(step)
 
+    @classmethod
+    def _generate_schema(cls, name: str, *steps: Step) -> None:
+        schema = cls(name)
+        for step in steps:
+            schema._add_step(step)
+        return schema
 
 PIPELINE_SCHEMAS = PipelineSchema._get_schemas()
