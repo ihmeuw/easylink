252a772e0b465054c1a32994f7eb7fa10490ce98
Patrick Nast <pnast@uw.edu>
2024-02-01 16:13:28 -0800

    adjust test

--- tests/unit/test_step.py
+++ tests/unit/test_step.py
@@ -10,59 +10,34 @@ def test_step_instantiation():
 
 def test_step_input():
     input_params = {
-        "env_var": "DUMMY_CONTAINER_MAIN_INPUT_FILE_PATHS",
-        "container_dir_name": "/input_data/main_input",
-        "host_filepaths": [
-            "/mnt/team/simulation_science/priv/engineering/er_ecosystem/sample_data/dummy/input_file_1.parquet"
-        ],
+        "env_var": "foo",
+        "container_dir_name": "/bar",
+        "host_filepaths": ["/baz/spam.csv"],
         "prev_output": False,
     }
 
     step_input = StepInput(**input_params)
-    assert step_input.container_paths == ["/input_data/main_input/input_file_1.parquet"]
-    assert step_input.bindings == {
-        "/mnt/team/simulation_science/priv/engineering/er_ecosystem/sample_data/dummy/input_file_1.parquet": "/input_data/main_input/input_file_1.parquet"
-    }
-    step_input.add_bindings(["/foo/bar"])
-    assert step_input.host_filepaths == [
-        "/mnt/team/simulation_science/priv/engineering/er_ecosystem/sample_data/dummy/input_file_1.parquet",
-        "/foo/bar",
-    ]
-    assert step_input.container_paths == [
-        "/input_data/main_input/input_file_1.parquet",
-        "/input_data/main_input/bar",
-    ]
-    assert step_input.bindings == {
-        "/mnt/team/simulation_science/priv/engineering/er_ecosystem/sample_data/dummy/input_file_1.parquet": "/input_data/main_input/input_file_1.parquet",
-        "/foo/bar": "/input_data/main_input/bar",
-    }
+    assert step_input.env_var == "foo"
+    assert step_input.container_dir_name == "/bar"
+    assert step_input.host_filepaths == ["/baz/spam.csv"]
+    assert step_input.prev_output is False
+    assert step_input.container_paths == ["/bar/spam.csv"]
+    assert step_input.bindings == {"/baz/spam.csv": "/bar/spam.csv"}
 
 
 def test_add_bindings_from_prev():
-    step = Step(
-        "foo",
-        validate_output=lambda x: x,
-        inputs={
-            "DUMMY_CONTAINER_MAIN_INPUT_FILE_PATHS": {
-                "container_dir_name": "/input_data/main_input",
-                "host_filepaths": [
-                    "/mnt/team/simulation_science/priv/engineering/er_ecosystem/sample_data/dummy/input_file_1.parquet"
-                ],
-                "prev_output": True,
-            }
-        },
-    )
-    step.add_bindings_from_prev(["/foo/bar"])
-    assert set(step.inputs[0].host_filepaths) == {
-        "/foo/bar",
-        "/mnt/team/simulation_science/priv/engineering/er_ecosystem/sample_data/dummy/input_file_1.parquet",
+    input_params = {
+        "foo": {
+            "container_dir_name": "/bar",
+            "host_filepaths": ["/baz/spam.csv"],
+            "prev_output": True,
+        }
     }
-    assert set(step.inputs[0].container_paths) == {
-        "/input_data/main_input/bar",
-        "/input_data/main_input/input_file_1.parquet",
+    step = Step("step1", validate_output=lambda x: x, inputs=input_params)
+    step.add_bindings_from_prev(["/previous_step/eggs.parquet"])
+    assert step.inputs[0].host_filepaths == ["/baz/spam.csv", "/previous_step/eggs.parquet"]
+    assert step.inputs[0].container_paths == ["/bar/spam.csv", "/bar/eggs.parquet"]
+    assert step.inputs[0].bindings == {
+        "/baz/spam.csv": "/bar/spam.csv",
+        "/previous_step/eggs.parquet": "/bar/eggs.parquet",
     }
-    for k, v in {
-        "/foo/bar": "/input_data/main_input/bar",
-        "/mnt/team/simulation_science/priv/engineering/er_ecosystem/sample_data/dummy/input_file_1.parquet": "/input_data/main_input/input_file_1.parquet",
-    }.items():
-        assert step.inputs[0].bindings[k] == v
