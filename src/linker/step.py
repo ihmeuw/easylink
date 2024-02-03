8fbf4e8402aa724a2345827b44247c34a98a8fd1
Patrick Nast <pnast@uw.edu>
2024-02-02 16:09:25 -0800

    change to input filenames

--- src/linker/step.py
+++ src/linker/step.py
@@ -7,10 +7,19 @@ from typing import Any, Callable, Dict, List
 class StepInput:
     """StepInput contains information about the input data for a step in a pipeline. It is used to bind input data to the container environment."""
 
-    env_var: str
-    container_dir_name: str
-    host_filepaths: List[str]
-    prev_output: bool
+    def __init__(
+        self,
+        env_var: str,
+        container_dir_name: str,
+        input_filenames: List[str],
+        prev_output: bool,
+        host_filepaths: List[str] = [],
+    ):
+        self.env_var = env_var
+        self.container_dir_name = container_dir_name
+        self.input_filenames = input_filenames
+        self.prev_output = prev_output
+        self.host_filepaths = host_filepaths
 
     @property
     def container_paths(self) -> List[str]:
@@ -29,6 +38,19 @@ class StepInput:
     def add_bindings(self, paths: List[Path]) -> None:
         self.host_filepaths.extend(paths)
 
+    def validate_input_filenames(self, input_data: Dict[str, Any]) -> Dict[str, str]:
+        errors = {}
+        for filename in self.input_filenames:
+            if filename not in input_data:
+                errors[
+                    filename
+                ] = f"Step requires input data key {filename} but it was not found in the input data."
+        return errors
+
+    def add_input_filename_bindings(self, input_data: Dict[str, Any]) -> None:
+        for filename in self.input_filenames:
+            self.host_filepaths.append(input_data[filename])
+
 
 class Step:
     """Steps contain information about the purpose of the interoperable elements of the sequence called a *Pipeline* and how those elements relate to one another.
@@ -46,3 +68,15 @@ class Step:
         input_from_prev = [input for input in self.inputs if input.prev_output]
         for input in input_from_prev:
             input.add_bindings(paths)
+
+    def validate_input_filenames(self, input_data: Dict[str, Any]) -> Dict[str, str]:
+        errors = {}
+        for input in self.inputs:
+            input_errors = input.validate_input_filenames(input_data)
+            if input_errors:
+                errors[input.env_var] = input_errors
+        return errors
+
+    def add_input_filename_bindings(self, input_data: Dict[str, Any]) -> None:
+        for input in self.inputs:
+            input.add_input_filename_bindings(input_data)
--- tests/unit/test_step.py
+++ tests/unit/test_step.py
@@ -12,32 +12,47 @@ def test_step_input():
     input_params = {
         "env_var": "foo",
         "container_dir_name": "/bar",
-        "host_filepaths": ["/baz/spam.csv"],
+        "input_filenames": ["step1"],
         "prev_output": False,
     }
 
     step_input = StepInput(**input_params)
     assert step_input.env_var == "foo"
     assert step_input.container_dir_name == "/bar"
-    assert step_input.host_filepaths == ["/baz/spam.csv"]
+    assert step_input.input_filenames == ["step1"]
     assert step_input.prev_output is False
-    assert step_input.container_paths == ["/bar/spam.csv"]
-    assert step_input.bindings == {"/baz/spam.csv": "/bar/spam.csv"}
+    assert step_input.host_filepaths == []
 
 
 def test_add_bindings_from_prev():
     input_params = {
         "foo": {
             "container_dir_name": "/bar",
-            "host_filepaths": ["/baz/spam.csv"],
+            "input_filenames": [],
             "prev_output": True,
         }
     }
     step = Step("step1", validate_output=lambda x: x, inputs=input_params)
     step.add_bindings_from_prev(["/previous_step/eggs.parquet"])
-    assert step.inputs[0].host_filepaths == ["/baz/spam.csv", "/previous_step/eggs.parquet"]
-    assert step.inputs[0].container_paths == ["/bar/spam.csv", "/bar/eggs.parquet"]
+    assert step.inputs[0].host_filepaths == ["/previous_step/eggs.parquet"]
+    assert step.inputs[0].container_paths == ["/bar/eggs.parquet"]
+    assert step.inputs[0].bindings == {
+        "/previous_step/eggs.parquet": "/bar/eggs.parquet",
+    }
+    
+def test_add_input_filename_bindings():
+    input_params = {
+        "foo": {
+            "container_dir_name": "/bar",
+            "input_filenames": ["foo"],
+            "host_filepaths": [],
+            "prev_output": True,
+        }
+    }
+    step = Step("step1", validate_output=lambda x: x, inputs=input_params)
+    step.add_input_filename_bindings({"foo":"/baz/spam.csv"})
+    assert step.inputs[0].host_filepaths == ["/baz/spam.csv"]
+    assert step.inputs[0].container_paths == ["/bar/spam.csv"]
     assert step.inputs[0].bindings == {
         "/baz/spam.csv": "/bar/spam.csv",
-        "/previous_step/eggs.parquet": "/bar/eggs.parquet",
     }
