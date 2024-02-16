217efe4659c8aeedf7f3773d12be62f03202fd76
Steve Bachmeier <sbachmei@uw.edu>
2024-02-16 10:41:51 -0800

    add test for exit_with_validation_error

---
+++ tests/unit/test_general_utils.py
@@ -0,0 +1,11 @@
+import errno
+
+import pytest
+
+from linker.utilities.general_utils import exit_with_validation_error
+
+
+def test_exit_with_validation_error():
+    with pytest.raises(SystemExit) as e:
+        exit_with_validation_error({"error": "error message"})
+    assert e.value.code == errno.EINVAL
