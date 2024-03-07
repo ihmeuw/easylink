57f80c559ba8631600682cab8d2acba033974e2e
Patrick Nast <pnast@uw.edu>
2024-03-07 09:27:35 -0800

    refactor where validations live

--- src/linker/utilities/data_utils.py
+++ src/linker/utilities/data_utils.py
@@ -4,10 +4,8 @@ import shutil
 from datetime import datetime
 from pathlib import Path
 from typing import Dict, List, Optional
-
-import pandas as pd
+from linker.configuration import Config
 import yaml
-from pyarrow import parquet as pq
 
 
 def create_results_directory(output_dir: Optional[str], timestamp: bool) -> Path:
@@ -42,22 +40,3 @@ def write_csv(filepath: str, rows: List) -> None:
     with open(filepath, "w") as file:
         writer = csv.writer(file)
         writer.writerows(rows)
-
-
-def validate_dummy_file(filepath: Path) -> None:
-    extension = filepath.suffix
-    if extension == ".parquet":
-        output_columns = set(pq.ParquetFile(filepath).schema.names)
-    elif extension == ".csv":
-        output_columns = set(pd.read_csv(filepath).columns)
-    else:
-        raise NotImplementedError(
-            f"Data file type {extension} is not compatible. Convert to Parquet or CSV instead"
-        )
-
-    required_columns = {"foo", "bar", "counter"}
-    missing_columns = required_columns - output_columns
-    if missing_columns:
-        raise LookupError(
-            f"Data file {filepath} is missing required column(s) {missing_columns}"
-        )
---
+++ src/linker/utilities/validation_utils.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+from pyarrow import parquet as pq
+import pandas as pd
+
+
+def validate_dummy_file(filepath: str) -> None:
+    extension = Path(filepath).suffix
+    if extension == ".parquet":
+        output_columns = set(pq.ParquetFile(filepath).schema.names)
+    elif extension == ".csv":
+        output_columns = set(pd.read_csv(filepath).columns)
+    else:
+        raise NotImplementedError(
+            f"Data file type {extension} is not compatible. Convert to Parquet or CSV instead"
+        )
+
+    required_columns = {"foo", "bar", "counter"}
+    missing_columns = required_columns - output_columns
+    if missing_columns:
+        raise LookupError(
+            f"Data file {filepath} is missing required column(s) {missing_columns}"
+        )
