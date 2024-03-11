2ec3508dd1560fd3857f6af2e19c226b7a4c0ebf
Patrick Nast <pnast@uw.edu>
2024-03-11 13:44:23 -0700

    added docstrings for rules

--- src/linker/rule.py
+++ src/linker/rule.py
@@ -4,6 +4,11 @@ from typing import Callable, List
 
 
 class Rule(ABC):
+    """
+    Abstract class to define interface between Steps and Implementations
+    and 'Rules' in Snakemake syntax that must be written to the Snakefile
+    """
+
     def write_to_snakefile(self, snakefile_dir) -> None:
         snakefile = snakefile_dir / "Snakefile"
         with open(snakefile, "a") as f:
@@ -11,11 +16,21 @@ class Rule(ABC):
 
     @abstractmethod
     def _build_rule(self) -> str:
+        "What actually gets written to the Snakefile. Must be implemented by subclasses."
         pass
 
 
 @dataclass
 class TargetRule(Rule):
+    """
+    A rule that defines the final output of the pipeline
+    Snakemake will determine the DAG based on this target.
+
+    Parameters:
+    target_files: List of file paths
+    validation: name of file created by InputValidationRule
+    """
+
     target_files: List[str]
     validation: str
 
@@ -29,6 +44,19 @@ rule all:
 
 @dataclass
 class ImplementedRule(Rule):
+    """
+    A rule that defines the execution of an implementation
+
+    Parameters:
+    name: Name
+    execution_input: List of file paths required by implementation
+    validation: name of file created by InputValidationRule to check for compatible input
+    output: List of file paths created by implementation
+    envvars: Dictionary of environment variables to set
+    diagnostics_dir: Directory for diagnostic files
+    script_cmd: Command to execute
+    """
+
     name: str
     execution_input: List[str]
     validation: str
@@ -68,6 +96,17 @@ rule:
 
 @dataclass
 class InputValidationRule(Rule):
+    """
+    A rule that validates input files against validator for
+    an implementation or the final output
+
+    Parameters:
+    name: Name
+    input: List of file paths to validate
+    output: file path to touch on successful validation. Must be used as an input for next rule.
+    validator: Callable that takes a file path as input. Raises an error if invalid.
+    """
+
     name: str
     input: List[str]
     output: str
