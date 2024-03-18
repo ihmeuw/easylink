69f1a9dd1889e7f6ddcc022d0b0f897aee7ca23d
Patrick Nast <pnast@uw.edu>
2024-03-18 11:25:12 -0700

    fix broken tests

--- tests/unit/test_runner.py
+++ tests/unit/test_runner.py
@@ -1,13 +1,14 @@
 from tempfile import TemporaryDirectory
 
 from linker.runner import get_environment_args, get_singularity_args
+from linker.utilities.paths import LINKER_TEMP
 
 
 def test_get_singularity_args(default_config, test_dir):
     with TemporaryDirectory() as results_dir:
         assert (
-            get_singularity_args(default_config.input_data, results_dir)
-            == f"--no-home --containall -B /tmp/linker:/tmp,"
+            get_singularity_args(default_config, results_dir)
+            == f"--no-home --containall -B {LINKER_TEMP[default_config.computing_environment]}:/tmp,"
             f"{results_dir},"
             f"{test_dir}/input_data1/file1.csv,"
             f"{test_dir}/input_data2/file2.csv"
