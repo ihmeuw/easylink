1d6be3d0fdf221b35b6483bec0eaaf5414665cb5
Patrick Nast <pnast@uw.edu>
2024-03-26 13:11:36 -0700

    remove todo

--- tests/unit/test_implementation.py
+++ tests/unit/test_implementation.py
@@ -1,12 +1,3 @@
-from pathlib import Path
-
 import pytest
 
 from linker.implementation import Implementation
-from linker.step import Step
-
-
-# TODO MIC-4921
-@pytest.mark.skip("Replace this with an integration test when appropriate")
-def test_fails_when_missing_results(default_config, mocker):
-    pass
