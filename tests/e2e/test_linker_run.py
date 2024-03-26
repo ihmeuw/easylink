d9cfe81b09f08f3e47d44a3b3ffba1e7c576bd1e
Steve Bachmeier <sbachmei@uw.edu>
2024-03-26 13:29:34 -0700

    put dirs back

--- tests/e2e/test_linker_run.py
+++ tests/e2e/test_linker_run.py
@@ -43,46 +43,45 @@ def test_linker_run(
     """e2e tests for 'linker run' command"""
     # Create a temporary directory to store results. We cannot use pytest's tmp_path fixture
     # because other nodes do not have access to it.
-    # with tempfile.TemporaryDirectory(dir="tests/e2e/") as results_dir:
-        # results_dir = Path(results_dir)
-    print(is_on_slurm())
-    results_dir = Path("/mnt/share/homes/sbachmei/venv/results")
-    cli_args = (
-        "run "
-        f"-p {SPECIFICATIONS_DIR / pipeline_specification} "
-        f"-i {SPECIFICATIONS_DIR / input_data} "
-        f"-e {SPECIFICATIONS_DIR / computing_environment} "
-        f"-o {str(results_dir)} "
-        "--no-timestamp"
-    )
-    # with capsys.disabled():  # disabled so we can monitor job submissions
-    #     print(
-    #         "\n\n*** RUNNING TEST ***\n"
-    #         f"[{pipeline_specification}, {input_data}, {computing_environment}]\n"
-    #     )
-    #     subprocess.run(
-    #         cmd,
-    #         shell=True,
-    #         stdout=sys.stdout,
-    #         stderr=sys.stderr,
-    #         check=True,
-    #     )
-    result = CliRunner().invoke(cli=cli.linker, args=cli_args)
-    print(result.stdout)  # useful for debugging in Jenkins console
-    assert result.exit_code == 0
-    assert (results_dir / "result.parquet").exists()
-    # Check that the results file checksum matches the expected value
-    with open(results_dir / "result.parquet", "rb") as f:
-        actual_checksum = hashlib.sha256(f.read()).hexdigest()
-    assert actual_checksum == RESULT_CHECKSUM
+    with tempfile.TemporaryDirectory(dir="tests/e2e/") as results_dir:
+        results_dir = Path(results_dir)
+        print(f"Is this on slurm: {is_on_slurm()}")
+        cli_args = (
+            "run "
+            f"-p {SPECIFICATIONS_DIR / pipeline_specification} "
+            f"-i {SPECIFICATIONS_DIR / input_data} "
+            f"-e {SPECIFICATIONS_DIR / computing_environment} "
+            f"-o {str(results_dir)} "
+            "--no-timestamp"
+        )
+        # with capsys.disabled():  # disabled so we can monitor job submissions
+        #     print(
+        #         "\n\n*** RUNNING TEST ***\n"
+        #         f"[{pipeline_specification}, {input_data}, {computing_environment}]\n"
+        #     )
+        #     subprocess.run(
+        #         cmd,
+        #         shell=True,
+        #         stdout=sys.stdout,
+        #         stderr=sys.stderr,
+        #         check=True,
+        #     )
+        result = CliRunner().invoke(cli=cli.linker, args=cli_args)
+        print(result.stdout)  # useful for debugging in Jenkins console
+        assert result.exit_code == 0
+        assert (results_dir / "result.parquet").exists()
+        # Check that the results file checksum matches the expected value
+        with open(results_dir / "result.parquet", "rb") as f:
+            actual_checksum = hashlib.sha256(f.read()).hexdigest()
+        assert actual_checksum == RESULT_CHECKSUM
 
-    assert (results_dir / pipeline_specification).exists()
-    assert (results_dir / input_data).exists()
-    assert (results_dir / computing_environment).exists()
+        assert (results_dir / pipeline_specification).exists()
+        assert (results_dir / input_data).exists()
+        assert (results_dir / computing_environment).exists()
 
-    # Check that implementation configuration worked
-    diagnostics_dir = results_dir / "diagnostics"
-    assert load_yaml(diagnostics_dir / "1_step_1" / "diagnostics.yaml")["increment"] == 1
-    assert (
-        load_yaml(diagnostics_dir / "2_step_2" / "diagnostics.yaml")["increment"] == 100
-    )
+        # Check that implementation configuration worked
+        diagnostics_dir = results_dir / "diagnostics"
+        assert load_yaml(diagnostics_dir / "1_step_1" / "diagnostics.yaml")["increment"] == 1
+        assert (
+            load_yaml(diagnostics_dir / "2_step_2" / "diagnostics.yaml")["increment"] == 100
+        )
