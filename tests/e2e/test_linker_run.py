dc2f22d5a3b55f34242314e0ffce315190df2c4d
Steve Bachmeier <sbachmei@uw.edu>
2024-03-26 15:42:25 -0700

    do not use context manager for e2e results_dir

--- tests/e2e/test_linker_run.py
+++ tests/e2e/test_linker_run.py
@@ -39,37 +39,40 @@ RESULTS_DIR = "/mnt/team/simulation_science/priv/engineering/scratch/"
 )
 def test_linker_run(pipeline_specification, input_data, computing_environment):
     """e2e tests for 'linker run' command"""
-    # Create a temporary directory to store results. We cannot use pytest's tmp_path fixture
-    # because other nodes do not have access to it.
     print(f"Is this on slurm: {is_on_slurm()}")  # print for jenkins
-    with tempfile.TemporaryDirectory(dir=RESULTS_DIR) as results_dir:
-        os.chmod(results_dir, os.stat(RESULTS_DIR).st_mode)
-        results_dir = Path(results_dir)
-        cli_args = (
-            "run "
-            f"-p {SPECIFICATIONS_DIR / pipeline_specification} "
-            f"-i {SPECIFICATIONS_DIR / input_data} "
-            f"-e {SPECIFICATIONS_DIR / computing_environment} "
-            f"-o {str(results_dir)} "
-            "--no-timestamp"
-        )
-        result = CliRunner().invoke(cli=cli.linker, args=cli_args)
-        print(result.output)  # print for jenkins
-        print(result.stdout)  # print for jenkins
-        assert result.exit_code == 0
-        assert (results_dir / "result.parquet").exists()
-        # Check that the results file checksum matches the expected value
-        with open(results_dir / "result.parquet", "rb") as f:
-            actual_checksum = hashlib.sha256(f.read()).hexdigest()
-        assert actual_checksum == RESULT_CHECKSUM
+    # Create a temporary directory to store results. We cannot use pytest's tmp_path fixture
+    # because other nodes do not have access to it. Also, do not use a context manager
+    # (i.e. tempfile.TemporaryDirectory) because it's too difficult to debug when the test
+    # fails b/c the dir gets deleted.
+    results_dir = tempfile.mkdtemp(dir=RESULTS_DIR)
+    os.chmod(results_dir, os.stat(RESULTS_DIR).st_mode)
+    results_dir = Path(results_dir)
+    cli_args = (
+        "run "
+        f"-p {SPECIFICATIONS_DIR / pipeline_specification} "
+        f"-i {SPECIFICATIONS_DIR / input_data} "
+        f"-e {SPECIFICATIONS_DIR / computing_environment} "
+        f"-o {str(results_dir)} "
+        "--no-timestamp"
+    )
+    result = CliRunner().invoke(cli=cli.linker, args=cli_args)
+    print(result.output)  # print for jenkins
+    print(result.stdout)  # print for jenkins
+    assert result.exit_code == 0
+    assert (results_dir / "result.parquet").exists()
+    # Check that the results file checksum matches the expected value
+    with open(results_dir / "result.parquet", "rb") as f:
+        actual_checksum = hashlib.sha256(f.read()).hexdigest()
+    assert actual_checksum == RESULT_CHECKSUM
 
-        assert (results_dir / pipeline_specification).exists()
-        assert (results_dir / input_data).exists()
-        assert (results_dir / computing_environment).exists()
+    assert (results_dir / pipeline_specification).exists()
+    assert (results_dir / input_data).exists()
+    assert (results_dir / computing_environment).exists()
 
-        # Check that implementation configuration worked
-        diagnostics_dir = results_dir / "diagnostics"
-        assert load_yaml(diagnostics_dir / "1_step_1" / "diagnostics.yaml")["increment"] == 1
-        assert (
-            load_yaml(diagnostics_dir / "2_step_2" / "diagnostics.yaml")["increment"] == 100
-        )
+    # Check that implementation configuration worked
+    diagnostics_dir = results_dir / "diagnostics"
+    assert load_yaml(diagnostics_dir / "1_step_1" / "diagnostics.yaml")["increment"] == 1
+    assert load_yaml(diagnostics_dir / "2_step_2" / "diagnostics.yaml")["increment"] == 100
+    
+    # If it made it through all this, delete the results_dir
+    os.system(f"rm -rf {results_dir}")
