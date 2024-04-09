c65fa15ad575a8597d7f1bde51f68b3865dda92e
Patrick Nast <pnast@uw.edu>
2024-04-09 11:50:50 -0700

    lint

--- src/linker/configuration.py
+++ src/linker/configuration.py
@@ -25,7 +25,7 @@ DEFAULT_ENVIRONMENT = {
     },
     "spark": {
         "workers": {
-            "num_workers": 2, 
+            "num_workers": 2,
             "cpus_per_node": 1,
             "mem_per_node": 1,  # GB
             "time_limit": 1,  # hours
