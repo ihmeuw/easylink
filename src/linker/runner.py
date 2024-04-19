76fc5c0d0d43f795fd97d32696eac97eae09f9c3
Patrick Nast <pnast@uw.edu>
2024-04-19 11:41:23 -0700

    make the variable a string instead

--- src/linker/runner.py
+++ src/linker/runner.py
@@ -42,7 +42,7 @@ def main(
     # We need to set a dummy environment variable to avoid logging a wall of text.
     # TODO [MIC-4920]: Remove when https://github.com/snakemake/snakemake-interface-executor-plugins/issues/55 merges
     # Set source cache in appropriate location so that jenkins runs
-    os.environ["XDG_CACHE_HOME"] = LINKER_TEMP[config.computing_environment] / "source_cache"
+    os.environ["XDG_CACHE_HOME"] = (LINKER_TEMP[config.computing_environment] / "source_cache").as_posix()
     os.environ["foo"] = "bar"
     argv = [
         "--snakefile",
