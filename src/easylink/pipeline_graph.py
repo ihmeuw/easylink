fa2d226eaf3c64844e85ec4081886da874d3af02
Patrick Nast <pnast@uw.edu>
2024-06-03 09:37:13 -0700

    add comments and docstrings

--- src/easylink/pipeline_graph.py
+++ src/easylink/pipeline_graph.py
@@ -15,13 +15,16 @@ class PipelineGraph(MultiDiGraph):
         self._create_graph(config)
 
     def _create_graph(self, config: Config) -> None:
+        """Create a graph from the pipeline configuration."""
         self.config = config
         self.add_node("input_data")
         prev_nodes = None
 
         for step in config.schema.steps:
             step_graph = step.get_subgraph(config)
+            # Source nodes are nodes with no incoming edges ('in_degree' = 0)
             step_source_nodes = [node for node, deg in step_graph.in_degree() if deg == 0]
+            # Add step graph to current graph without connections
             self.update(step_graph)
             for source_node in step_source_nodes:
                 # Connect new source nodes to old sink nodes
@@ -40,25 +43,31 @@ class PipelineGraph(MultiDiGraph):
                         source_node,
                         files=[str(file) for file in config.input_data],
                     )
-            prev_nodes = [node for node, deg in self.out_degree() if deg == 0]
-        self.add_node("results")
 
+            # Set prev_nodes to the sink nodes of the current step
+            prev_nodes = [node for node, deg in self.out_degree() if deg == 0]
+        # Add results node and connect to final sink nodes
+        self.add_node("results")
         for node in prev_nodes:
             self.add_edge(node, "results", files=["result.parquet"])
 
     @property
     def implementation_nodes(self) -> List[str]:
+        """Return list of nodes tied to specific implementations."""
         ordered_nodes = list(nx.topological_sort(self))
         return [node for node in ordered_nodes if node != "input_data" and node != "results"]
 
     @property
     def implementations(self) -> List[Implementation]:
+        """Convenience property to get all implementations in the graph."""
         return [self.get_attr(node, "implementation") for node in self.implementation_nodes]
 
     def get_attr(self, node: str, attr: str) -> Any:
+        """Convenience method to get a particular attribute from a node"""
         return self.nodes[node][attr]
 
     def get_input_output_files(self, node: str) -> Tuple[List[str], List[str]]:
+        """Get all of a node's input and output files from edges."""
         input_files = list(
             itertools.chain.from_iterable(
                 [data["files"] for _, _, data in self.in_edges(node, data=True)]
