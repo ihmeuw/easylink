513d2a5a2eb1aca0b31b8d3fb7f740b0af4f2d38
Patrick Nast <pnast@uw.edu>
2024-06-06 12:07:14 -0700

    create abstractstep

--- src/easylink/step.py
+++ src/easylink/step.py
@@ -1,17 +1,44 @@
-from dataclasses import dataclass
+from abc import ABC, abstractmethod
+from pathlib import Path
 from typing import TYPE_CHECKING, Callable
 
 from networkx import MultiDiGraph
 
 from easylink.implementation import Implementation
-from easylink.utilities.validation_utils import validate_input_file_dummy
 
 if TYPE_CHECKING:
     from easylink.configuration import Config
 
 
-@dataclass
-class Step:
+class AbstractStep(ABC):
+    def __init__(self, name: str, input_validator: Callable, out_dir: Path) -> None:
+        self.name = name
+        self.input_validator = input_validator
+        self.out_dir = out_dir
+
+    @abstractmethod
+    def get_subgraph(self, config: "Config") -> MultiDiGraph:
+        pass
+
+class InputStep(AbstractStep):
+    def get_subgraph(self, config: "Config") -> MultiDiGraph:
+        sub_graph = MultiDiGraph()
+        sub_graph.add_node(
+            "input_data", input_validator=self.input_validator, out_dir=self.out_dir
+        )
+        return sub_graph
+
+
+class ResultStep(AbstractStep):
+    def get_subgraph(self, config: "Config") -> MultiDiGraph:
+        sub_graph = MultiDiGraph()
+        sub_graph.add_node(
+            "results", input_validator=self.input_validator, out_dir=self.out_dir
+        )
+        return sub_graph
+
+
+class ImplementedStep(AbstractStep):
     """Steps contain information about the purpose of the interoperable elements of
     the sequence called a *Pipeline* and how those elements relate to one another.
     In turn, steps are implemented by Implementations, such that each step may have
@@ -19,14 +46,24 @@ class Step:
     In a sense, steps contain metadata about the implementations to which they relate.
     """
 
-    name: str
-    input_validator: Callable
+    def __init__(self, name: str, input_validator: Callable, out_dir: Path) -> None:
+        self.name = name
+        self.input_validator = input_validator
+        self.out_dir = out_dir
 
     def get_subgraph(self, config: "Config") -> MultiDiGraph:
         sub = MultiDiGraph()
+        implementation_name = config.get_implementation_name(self.name)
+        implementation_config = config.pipeline[self.name]["implementation"]["configuration"]
+        implementation = Implementation(
+            name=implementation_name,
+            step_name=self.name,
+            environment_variables=implementation_config.to_dict(),
+        )
         sub.add_node(
-            config.schema.get_step_id(self),
-            implementation=Implementation(config=config, step=self),
+            implementation_name,
+            implementation=implementation,
             input_validator=self.input_validator,
+            out_dir=self.out_dir / implementation_name,
         )
         return sub
