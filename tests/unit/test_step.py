4f656428ecc1bdf0bd317a9060ef1abf458972b5
Patrick Nast <pnast@uw.edu>
2024-06-11 16:02:04 -0700

    add test coverage for new steps

--- tests/unit/test_step.py
+++ tests/unit/test_step.py
@@ -1,28 +1,95 @@
+from pathlib import Path
 from typing import Callable
 
 from easylink.implementation import Implementation
-from easylink.step import ImplementedStep
+from easylink.pipeline_schema_constants import validate_input_file_dummy
+from easylink.step import CompositeStep, ImplementedStep, InputStep, ResultStep
 
 
-def test_step_instantiation():
+def test_input_step(default_config):
     params = {
-        "input_validator": lambda *_: None,
-        "out_dir": "results",
+        "input_validator": validate_input_file_dummy,
+        "out_dir": Path("input"),
     }
-    step = ImplementedStep("foo", **params)
-    assert step.name == "foo"
-    assert step.out_dir == "results"
+    step = InputStep("input", **params)
+    assert step.name == "input"
+    assert step.out_dir == Path("input")
+
+    # Test get_subgraph
+    subgraph = step.get_subgraph(default_config)
+    assert list(subgraph.nodes) == ["input_data"]
+    assert list(subgraph.edges) == []
+    assert subgraph.nodes["input_data"]["input_validator"] == validate_input_file_dummy
+    assert subgraph.nodes["input_data"]["out_dir"] == Path("input")
+
+
+def test_result_step(default_config):
+    params = {
+        "input_validator": validate_input_file_dummy,
+        "out_dir": Path("results"),
+    }
+    step = ResultStep("results", **params)
+    assert step.name == "results"
+    assert step.out_dir == Path("results")
+
+    # Test get_subgraph
+    subgraph = step.get_subgraph(default_config)
+    assert list(subgraph.nodes) == ["results"]
+    assert list(subgraph.edges) == []
+    assert subgraph.nodes["results"]["input_validator"] == validate_input_file_dummy
+    assert subgraph.nodes["results"]["out_dir"] == Path("results")
+
+
+def test_implemented_step(default_config):
+    params = {
+        "input_validator": validate_input_file_dummy,
+        "out_dir": Path("foo"),
+    }
+    step = ImplementedStep("step_1", **params)
+
+    assert step.name == "step_1"
+    assert step.out_dir == Path("foo")
     assert isinstance(step.input_validator, Callable)
 
+    # Test get_subgraph
+    subgraph = step.get_subgraph(default_config)
+    imp_node = f"{step.name}_python_pandas"
+    assert list(subgraph.nodes) == [imp_node]
+    assert list(subgraph.edges) == []
+    assert subgraph.nodes[imp_node]["input_validator"] == validate_input_file_dummy
+    assert subgraph.nodes[imp_node]["out_dir"] == Path("foo") / imp_node
+    implementation = subgraph.nodes[imp_node]["implementation"]
+    assert isinstance(implementation, Implementation)
+    assert implementation.name == imp_node
+    assert implementation.schema_step_name == step.name
 
-def test_get_subgraph(default_config):
-    schema = default_config.schema
-    for step in schema.steps:
-        subgraph = step.get_subgraph(default_config)
-        assert len(subgraph.nodes) == 1
-        assert len(subgraph.edges) == 0
-        imp_node = f"{step.name}_python_pandas"
-        implementation = subgraph.nodes[imp_node]["implementation"]
-        assert isinstance(implementation, Implementation)
-        assert implementation.name == imp_node
-        assert implementation.schema_step_name == step.name
+
+def test_composite_step(default_config):
+    params = {
+        "step_1": {
+            "step_type": ImplementedStep,
+            "step_params": {
+                "input_validator": validate_input_file_dummy,
+                "out_dir": Path("intermediate"),
+            },
+        },
+    }
+    step = CompositeStep("composite", **params)
+    assert step.name == "composite"
+    assert step.out_dir == Path()
+
+    # Test get_subgraph
+    subgraph = step.get_subgraph(default_config)
+    assert list(subgraph.nodes) == ["step_1_python_pandas"]
+    assert list(subgraph.edges) == []
+    assert (
+        subgraph.nodes["step_1_python_pandas"]["input_validator"] == validate_input_file_dummy
+    )
+    assert (
+        subgraph.nodes["step_1_python_pandas"]["out_dir"]
+        == Path("intermediate") / "step_1_python_pandas"
+    )
+    implementation = subgraph.nodes["step_1_python_pandas"]["implementation"]
+    assert isinstance(implementation, Implementation)
+    assert implementation.name == "step_1_python_pandas"
+    assert implementation.schema_step_name == "step_1"
