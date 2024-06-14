55e935a105d18c4531d25fb0bfad3d115c94de9b
Patrick Nast <pnast@uw.edu>
2024-06-14 16:59:08 -0700

    refactor schema constants

--- src/easylink/pipeline_schema_constants.py
+++ src/easylink/pipeline_schema_constants.py
@@ -3,127 +3,74 @@ from pathlib import Path
 from easylink.step import CompositeStep, ImplementedStep, InputStep, ResultStep
 from easylink.utilities.validation_utils import validate_input_file_dummy
 
-ALLOWED_SCHEMA_PARAMS = {
-    "development": {
-        "input_data_schema": {
-            "step_type": InputStep,
-            "step_params": {
-                "input_validator": validate_input_file_dummy,
-                "out_dir": Path(),
-            },
-            "in_edges": {},
+SCHEMA_NODES = [
+    InputStep("input_data_schema", input_slots=[], output_slots=["file1"]),
+    CompositeStep(
+        "step_1",
+        input_slots=[("step_1_main_input", None, validate_input_file_dummy)],
+        output_slots=["step_1_main_output"],
+        nodes=[
+            ImplementedStep(
+                "step_1a",
+                input_slots=[("step_1a_main_input", None, validate_input_file_dummy)],
+                output_slots=["step_1a_main_output"],
+            ),
+            ImplementedStep(
+                "step_1b",
+                input_slots=[("step_1b_main_input", None, validate_input_file_dummy)],
+                output_slots=["step_1b_main_output"],
+            ),
+        ],
+        edges=[("step_1a", "step_1b", "step_1a_main_output", "step_1b_main_input")],
+        slot_mappings={
+            "input": [("step_1a", "step_1_main_input", "step_1a_main_input")],
+            "output": [("step_1b", "step_1_main_output", "step_1b_main_output")],
         },
-        "step_1": {
-            "step_type": CompositeStep,
-            "step_params": {
-                "step_1": {
-                    "step_type": ImplementedStep,
-                    "step_params": {
-                        "input_validator": validate_input_file_dummy,
-                        "out_dir": Path("intermediate"),
-                    },
-                },
-            },
-            "in_edges": {
-                "input_data_schema": {
-                    "env_var": "DUMMY_CONTAINER_MAIN_INPUT_FILE_PATHS",
-                    "files": ["file1"],
-                }
-            },
-        },
-        "step_2": {
-            "step_type": ImplementedStep,
-            "step_params": {
-                "input_validator": validate_input_file_dummy,
-                "out_dir": Path("intermediate"),
-            },
-            "in_edges": {
-                "step_1": {
-                    "env_var": "DUMMY_CONTAINER_MAIN_INPUT_FILE_PATHS",
-                    "files": ["result.parquet"],
-                }
-            },
-        },
-        "step_3": {
-            "step_type": ImplementedStep,
-            "step_params": {
-                "input_validator": validate_input_file_dummy,
-                "out_dir": Path("intermediate"),
-            },
-            "in_edges": {
-                "step_2": {
-                    "env_var": "DUMMY_CONTAINER_MAIN_INPUT_FILE_PATHS",
-                    "files": ["result.parquet"],
-                }
-            },
-        },
-        "step_4": {
-            "step_type": ImplementedStep,
-            "step_params": {
-                "input_validator": validate_input_file_dummy,
-                "out_dir": Path("results"),
-            },
-            "in_edges": {
-                "step_3": {
-                    "env_var": "DUMMY_CONTAINER_MAIN_INPUT_FILE_PATHS",
-                    "files": ["result.parquet"],
-                },
-                "input_data_schema": {
-                    "env_var": "DUMMY_CONTAINER_SECONDARY_INPUT_FILE_PATHS",
-                    "files": ["file1"],
-                },
-            },
-        },
-        "results_schema": {
-            "step_type": ResultStep,
-            "step_params": {
-                "input_validator": validate_input_file_dummy,
-                "out_dir": Path(),
-            },
-            "in_edges": {
-                "step_4": {
-                    "env_var": "DUMMY_CONTAINER_MAIN_INPUT_FILE_PATHS",
-                    "files": ["result.parquet"],
-                },
-            },
-        },
-    },
-}
-TESTING_SCHEMA_PARAMS = {
-    "integration": {
-        "input_data_schema": {
-            "step_type": InputStep,
-            "step_params": {
-                "input_validator": validate_input_file_dummy,
-                "out_dir": Path(),
-            },
-            "in_edges": {},
-        },
-        "step_1": {
-            "step_type": ImplementedStep,
-            "step_params": {
-                "input_validator": validate_input_file_dummy,
-                "out_dir": Path("results"),
-            },
-            "in_edges": {
-                "input_data_schema": {
-                    "env_var": "DUMMY_CONTAINER_MAIN_INPUT_FILE_PATHS",
-                    "files": ["file1"],
-                }
-            },
-        },
-        "results_schema": {
-            "step_type": ResultStep,
-            "step_params": {
-                "input_validator": validate_input_file_dummy,
-                "out_dir": Path(),
-            },
-            "in_edges": {
-                "step_1": {
-                    "env_var": "DUMMY_CONTAINER_MAIN_INPUT_FILE_PATHS",
-                    "files": ["result.parquet"],
-                },
-            },
-        },
-    },
-}
+    ),
+    ImplementedStep(
+        "step_2",
+        input_slots=[
+            (
+                "step_2_main_input",
+                "DUMMY_CONTAINER_MAIN_INPUT_FILE_PATHS",
+                validate_input_file_dummy,
+            )
+        ],
+        output_slots=["step_2_main_output"],
+    ),
+    ImplementedStep(
+        "step_3",
+        input_slots=[
+            (
+                "step_3_main_input",
+                "DUMMY_CONTAINER_MAIN_INPUT_FILE_PATHS",
+                validate_input_file_dummy,
+            )
+        ],
+        output_slots=["step_3_main_output"],
+    ),
+    ImplementedStep(
+        "step_4",
+        input_slots=[
+            (
+                "step_4_main_input",
+                "DUMMY_CONTAINER_MAIN_INPUT_FILE_PATHS",
+                validate_input_file_dummy,
+            )
+        ],
+        output_slots=["step_4_main_output"],
+    ),
+    ResultStep(
+        "results_schema",
+        input_slots=[("result", None, validate_input_file_dummy)],
+        output_slots=[],
+    ),
+]
+SCHEMA_EDGES = [
+    ("input_data_schema", "step_1", "file1", "step_1_main_input"),
+    ("step_1", "step_2", "step_1_main_output", "step_2_main_input"),
+    ("step_2", "step_3", "step_2_main_output", "step_3_main_input"),
+    ("step_3", "step_4", "step_3_main_output", "step_4_main_input"),
+    ("step_4", "results_schema", "step_4_main_output", "result"),
+]
+ALLOWED_SCHEMA_PARAMS = {"development": (SCHEMA_NODES, SCHEMA_EDGES)}
