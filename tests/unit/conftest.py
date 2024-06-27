e1b636f77ab5a30ccecae014042d6ea07e8b4f54
Patrick Nast <pnast@uw.edu>
2024-06-27 14:46:01 -0700

    remove spurious key

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -192,9 +192,6 @@ PIPELINE_CONFIG_DICT = {
     },
     "missing_substep_keys": {
         "step_1": {
-            "implementation": {
-                "name": "step_1_python_pandas",
-            },
             "substeps": {
                 "step_1a": {
                     "implementation": {},
