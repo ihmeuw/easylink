4244521f68594658e349553b3201c612404828cd
Patrick Nast <pnast@uw.edu>
2024-06-28 13:44:05 -0700

    remove default configuration of implementation config

--- src/easylink/step.py
+++ src/easylink/step.py
@@ -87,17 +87,16 @@ class BasicStep(Step):
         self, graph: nx.MultiDiGraph, step_config: LayeredConfigTree
     ) -> None:
         """Return a single node with an implementation attribute."""
-        implementation_name = step_config[self.name]["implementation"]["name"]
-        implementation_config = step_config[self.name]["implementation"]["configuration"]
+        implementation_config = step_config[self.name]["implementation"]
+        implementation_name = implementation_config["name"]
         implementation_node_name = (
             implementation_name
             if self.name == self.step_name
             else f"{self.name}_{implementation_name}"
         )
         implementation = Implementation(
-            name=implementation_name,
             step_name=self.step_name,
-            environment_variables=implementation_config.to_dict(),
+            implementation_config=implementation_config.to_dict(),
         )
         graph.add_node(
             implementation_node_name,
@@ -391,8 +390,8 @@ class LoopStep(CompositeStep, BasicStep):
 
     def get_loop_config(
         self, iterate_config: LayeredConfigTree
-    ) -> Dict[str, LayeredConfigTree]:
-        loop_config = {}
+    ) -> LayeredConfigTree:
+        loop_config = LayeredConfigTree()
         for i, loop in enumerate(iterate_config):
-            loop_config[f"{self.name}_loop_{i+1}"] = loop
+            loop_config.update({f"{self.name}_loop_{i+1}": loop})
         return loop_config
