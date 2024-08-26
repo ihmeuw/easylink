724efe3c93203a07fc9b419d4e7d5b64bc682b24
Patrick Nast <pnast@uw.edu>
2024-08-26 15:01:50 -0700

    add validations

--- src/easylink/utilities/validation_utils.py
+++ src/easylink/utilities/validation_utils.py
@@ -22,6 +22,38 @@ def validate_input_file_dummy(filepath: str) -> None:
             f"Data file {filepath} is missing required column(s) {missing_columns}"
         )
 
-
+DEMO_SAMPLE_PATH = Path(__file__).parent.parent.parent.parent / "sample_data/pvs_like_case_study/simulated_census_2030.parquet"
 def demo_validator(filepath: str) -> None:
-    pass
+    if Path(filepath).name == "simulated_geobase_reference_file.parquet":
+        return
+    
+    sample_df = pd.read_parquet(DEMO_SAMPLE_PATH)
+    extension = Path(filepath).suffix
+    if not extension == ".parquet":
+        raise NotImplementedError(
+            f"Data file type {extension} is not supported. Convert to Parquet instead"
+        )
+    df_to_validate = pd.read_parquet(filepath)
+    
+    val_cols = set(df_to_validate.columns)
+    expected_cols = set(sample_df.columns)
+    if val_cols.symmetric_difference(expected_cols) - {"pik"}:
+        err_str = f"Data file {filepath}"
+        excess_cols = val_cols.difference(expected_cols) - {"pik"}
+        missing_cols = expected_cols.difference(val_cols)
+        if excess_cols:
+            err_str += f" has excess columns {excess_cols}"
+            if missing_cols: err_str += " and"
+        if missing_cols:
+            err_str += f" is missing columns {missing_cols}"
+        
+        raise LookupError(err_str)
+        
+    
+    if len(df_to_validate.index) != len(sample_df.index):
+        raise ValueError(
+            f"Data file {filepath} has {len(df_to_validate.index)} rows but {len(sample_df.index)} are expected."
+        )
+    
+    
+    
