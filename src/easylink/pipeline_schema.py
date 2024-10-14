b61497132c3a408f3ced46ae6afc7f7aa9771133
Patrick Nast <pnast@uw.edu>
2024-10-14 13:46:00 -0700

    lint

--- src/easylink/pipeline_schema.py
+++ src/easylink/pipeline_schema.py
@@ -5,7 +5,7 @@ from layered_config_tree import LayeredConfigTree
 
 from easylink.graph_components import EdgeParams
 from easylink.pipeline_schema_constants import ALLOWED_SCHEMA_PARAMS
-from easylink.step import NonLeafConfigurationState, HierarchicalStep, Step
+from easylink.step import HierarchicalStep, NonLeafConfigurationState, Step
 
 
 class PipelineSchema(HierarchicalStep):
