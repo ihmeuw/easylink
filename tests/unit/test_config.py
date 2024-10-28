c71e1159b3e27250a5fe2b815b7c3045123b0d07
Patrick Nast <pnast@uw.edu>
2024-10-28 14:03:06 -0700

    rename test dict

--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -198,10 +198,10 @@ def test_spark_requests(default_config_params, input, requires_spark):
 
 @pytest.mark.parametrize("is_default", [True, False])
 def test_combined_implementations(default_config_params, is_default):
-    non_default_dict = {"foo": "bar"}
+    combined_dict = {"foo": "bar"}
     config_params = default_config_params
     if not is_default:
-        config_params["pipeline"]["combined_implementations"] = non_default_dict
+        config_params["pipeline"]["combined_implementations"] = combined_dict
     combined_implementations = Config(config_params).pipeline.combined_implementations
-    expected = {} if is_default else non_default_dict
+    expected = {} if is_default else combined_dict
     assert combined_implementations.to_dict() == expected
