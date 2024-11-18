9431493d1d6e95596e804ba136592ccaf6ba4516
Patrick Nast <pnast@uw.edu>
2024-11-18 10:39:38 -0800

    adjsut test.py to handle files in new implementation

--- src/easylink/steps/dev/test.py
+++ src/easylink/steps/dev/test.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import subprocess
+from pathlib import Path
 
 # This script just tests that the dummy containers can actually be iterated (each taking as input the
 # output of the previous). It is not part of the dummy containers themselves!
@@ -30,11 +31,9 @@ rm_in_directory(singularity_tmp)
 rm_in_directory(singularity_workdir)
 rm_in_directory(results_dir)
 
-input_file = os.getenv("DUMMY_CONTAINERS_TEST_INPUT_FILE", "./input_file_2.csv")
-input_files = [input_file]
+input_file = os.getenv("DUMMY_CONTAINERS_TEST_INPUT_FILE", "./input_data/input_file_2.csv")
+input_files = [str(Path(input_file).resolve())]
 input_file_format = input_file.split(".")[-1]
-
-container_engine = os.getenv("DUMMY_CONTAINERS_TEST_CONTAINER_ENGINE", "singularity")
 
 # Useful for debugging: you don't need to rebuild the containers for script changes if you use this flag.
 use_latest_scripts = os.getenv(
@@ -47,7 +46,11 @@ import random
 
 random.seed(1234)
 
-implementations = ["python_pandas", "r", "python_pyspark"]
+implementations = [
+    "python_pandas/python_pandas.sif",
+    "r/r-image.sif",
+    "python_pyspark/python_pyspark.sif",
+]
 
 steps = [
     {
@@ -56,19 +59,6 @@ steps = [
     }
     for _ in range(num_steps)
 ]
-
-if container_engine == "docker" and os.getenv(
-    "DUMMY_CONTAINERS_TEST_FROM_TAR", "false"
-).lower() in ("true", "1", "t"):
-    for implementation in implementations:
-        command = [
-            "docker",
-            "load",
-            "-i",
-            f'./{implementation}/{implementation.replace("_", "-")}-image.tar.gz',
-        ]
-        print(" ".join(command))
-        subprocess.check_output(command)
 
 files_so_far = input_files.copy()
 
@@ -93,7 +83,9 @@ for i, step in enumerate(steps):
     else:
         output_files = [f"{step_results_dir}/result.{output_file_format}"]
 
+    input_env_vars = ["DUMMY_CONTAINER_MAIN_INPUT_FILE_PATHS"]
     env_var_args = {
+        "SPARK_MASTER_URL": "local[2]",
         "OUTPUT_FILE_FORMAT": output_file_format,
         "OUTPUT_PATHS": ",".join(
             [
@@ -131,11 +123,8 @@ for i, step in enumerate(steps):
         else:
             raise ValueError()
 
-    for index, input_file in enumerate(input_files):
-        input_path_inside_container = (
-            f"/input_data/main_input_file_{index}.{input_file_format}"
-        )
-        bindings.append((input_file, input_path_inside_container))
+    bindings.extend([(input_file, input_file) for input_file in input_files])
+    env_var_args["MAIN_INPUT_FILE_PATHS"] = ",".join(input_files)
 
     if (
         os.getenv("DUMMY_CONTAINERS_TEST_INCLUDE_EXTRA_INPUTS", "false").lower()
@@ -144,10 +133,8 @@ for i, step in enumerate(steps):
     ):
         second_main_input_file = random.choice(files_so_far)
         second_main_input_file_format = second_main_input_file.split(".")[-1]
-        path_inside_container = (
-            f"/input_data/main_input_file_2.{second_main_input_file_format}"
-        )
-        bindings.append((second_main_input_file, path_inside_container))
+        env_var_args["MAIN_INPUT_FILE_PATHS"] += "," + second_main_input_file
+        bindings.append((second_main_input_file, second_main_input_file))
 
     if (
         os.getenv("DUMMY_CONTAINERS_TEST_INCLUDE_EXTRA_INPUTS", "false").lower()
@@ -156,9 +143,9 @@ for i, step in enumerate(steps):
     ):
         secondary_input_file = random.choice(files_so_far)
         secondary_input_file_format = secondary_input_file.split(".")[-1]
-        path_inside_container = f"/input_data/secondary_input.{secondary_input_file_format}"
-        env_var_args["SECONDARY_INPUT_FILE_PATHS"] = path_inside_container
-        bindings.append((secondary_input_file, path_inside_container))
+        env_var_args["SECONDARY_INPUT_FILE_PATHS"] = secondary_input_file
+        input_env_vars.append("DUMMY_CONTAINER_SECONDARY_INPUT_FILE_PATHS")
+        bindings.append((secondary_input_file, secondary_input_file))
 
     if (
         os.getenv("DUMMY_CONTAINERS_TEST_INCLUDE_EXTRA_INPUTS", "false").lower()
@@ -169,9 +156,12 @@ for i, step in enumerate(steps):
         implementation_specific_input_file_format = implementation_specific_input_file.split(
             "."
         )[-1]
-        path_inside_container = f"/extra_implementation_specific_input_data/input.{implementation_specific_input_file_format}"
-        env_var_args["EXTRA_IMPLEMENTATION_SPECIFIC_INPUT_FILE_PATH"] = path_inside_container
-        bindings.append((implementation_specific_input_file, path_inside_container))
+        env_var_args["EXTRA_IMPLEMENTATION_SPECIFIC_INPUT_FILE_PATH"] = (
+            implementation_specific_input_file
+        )
+        bindings.append(
+            (implementation_specific_input_file, implementation_specific_input_file)
+        )
 
     if os.getenv("DUMMY_CONTAINERS_TEST_INCLUDE_EXTRA_OUTPUTS", "false").lower() in (
         "true",
@@ -190,33 +180,18 @@ for i, step in enumerate(steps):
 
     env = {**os.environ}
 
-    if container_engine == "singularity":
-        image = f'./{implementation}/{implementation.replace("_", "-")}-image.sif'
-        command = [
-            "singularity",
-            "run",
-            "--pwd",
-            workdir,
-            "-B",
-            ",".join([f"{k}:{v}" for k, v in bindings]),
-            image,
-        ]
-        env = {**env, **{f"SINGULARITYENV_{k}": v for k, v in env_var_args.items()}}
-    elif container_engine == "docker":
-        image = f"linker:dummy_container_{implementation}"
-        command = [
-            "docker",
-            "run",
-        ]
-        for k, v in bindings:
-            command += ["--mount", f"type=bind,source={k},target={v}"]
-
-        for k, v in env_var_args.items():
-            command += ["-e", f"{k}={v}"]
-
-        command += ["-i", "-t", image]
-    else:
-        raise ValueError()
+    image = str(Path(f"./{implementation}").resolve())
+    command = [
+        "singularity",
+        "run",
+        "--pwd",
+        workdir,
+        "-B",
+        ",".join([f"{k}:{v}" for k, v in bindings]),
+        image,
+    ]
+    env = {**env, **{f"SINGULARITYENV_{k}": v for k, v in env_var_args.items()}}
+    env["INPUT_ENV_VARS"] = ",".join(input_env_vars)
 
     print(" ".join(command))
     print(env_var_args)
