98f9248a15cf31f96f4cac2d89311176eab171ab
Patrick Nast <pnast@uw.edu>
2024-11-18 09:43:36 -0800

    fix broken names

--- src/easylink/steps/dev/build-containers-local.sh
+++ src/easylink/steps/dev/build-containers-local.sh
@@ -3,10 +3,10 @@
 set -e
 
 cd python_pandas
-sudo singularity build --force python-pandas-image.sif python-pandas.def
+sudo singularity build --force python_pandas.sif python_pandas.def
 
 cd ../python_pyspark
-sudo singularity build --force python-pyspark-image.sif python-pyspark.def
+sudo singularity build --force python_pyspark.sif python_pyspark.def
 
 cd ../r
 sudo singularity build --force r-image.sif r-image.def
