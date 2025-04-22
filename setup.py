cb1ae5a4c8cb57016e35b1877a9c3c9362d221cf
Tyler Yandrofski <tylerdy@uw.edu>
2025-04-22 11:36:14 -0400

    Add initial notes on easylink install/ running (#167)
    Initial notes on easylink install and running with test configs
    
    ---------
    
    Co-authored-by: tylerdy <tylerdy@long-slurm-sarchive-p0001.cluster.ihme.washington.edu>
    Co-authored-by: tylerdy <tylerdy@long-slurm-sarchive-p0045.cluster.ihme.washington.edu>
    Co-authored-by: Zeb Burke-Conte <zmbc@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -71,6 +71,7 @@ if __name__ == "__main__":
         "sphinx-rtd-theme",
         "sphinx-autodoc-typehints",
         "sphinx-click",
+        "sphinx-autobuild",
         "typing_extensions",
     ]
     lint_requirements = [
