451dc76b577384381bf5e10a0784ad767d180b13
Zeb Burke-Conte <zmbc@uw.edu>
2025-05-12 16:38:31 -0700

    Add a failing test for an output slot being directory-valued

---
+++ src/easylink/steps/output_dir/dummy_step_2_for_output_dir_example.py
@@ -0,0 +1,21 @@
+# PIPELINE_SCHEMA: output_dir
+# STEP_NAME: step_2_for_output_dir_example
+# REQUIREMENTS: pandas==2.1.2 pyarrow
+
+import pandas as pd
+import shutil
+import os
+from pathlib import Path
+
+dir_path = Path(os.environ["DUMMY_CONTAINER_MAIN_INPUT_DIR_PATH"])
+saved = False
+
+for i, f in enumerate([f for f in dir_path.iterdir() if f.is_file()]):
+    if "snakemake" in str(f):
+        continue
+
+    if not saved:
+        shutil.copy(f, os.environ["DUMMY_CONTAINER_OUTPUT_PATHS"])
+        saved = True
+
+    print(pd.read_parquet(f))
