3930c002692fc0e291c9d9ca64a8607ab643e8dd
Tyler Yandrofski <tylerdy@uw.edu>
2025-05-20 15:00:46 -0400

    Dummy implementations (#201)
    * Dummy implementations and input data
    * Rename env var `CLUSTERS_FILE_PATHS` to `CLUSTERS_FILE_PATH`

---
+++ src/easylink/steps/rl-dummy/determining_exclusions/dummy_determining_exclusions.py
@@ -0,0 +1,49 @@
+# STEP_NAME: determining_exclusions
+
+# REQUIREMENTS: pandas==2.1.2 pyarrow pyyaml
+
+# PIPELINE_SCHEMA: main
+
+import logging
+import os
+
+import pandas as pd
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(message)s",
+    handlers=[logging.StreamHandler()],
+)
+
+# LOAD INPUTS
+
+# INPUT_DATASETS_AND_INPUT_KNOWN_CLUSTERS_FILE_PATHS is list of filepaths which includes
+# the known_clusters filepath due to workaround
+dataset_paths = os.environ["INPUT_DATASETS_AND_INPUT_KNOWN_CLUSTERS_FILE_PATHS"].split(",")
+dataset_paths = [path for path in dataset_paths if "known_clusters.parquet" not in path]
+
+# for workaround, choose path based on INPUT_DATASETS_SPLITTER_CHOICE configuration
+splitter_choice = os.environ["INPUT_DATASETS_SPLITTER_CHOICE"]
+dataset_path = ""
+for path in dataset_paths:
+    if splitter_choice == os.path.basename(path):
+        dataset_path = path
+        break
+if dataset_path == "":
+    raise ValueError()
+
+# don't need to actually this dataset or the clusters dataset,
+# we will just save an empty ids_to_remove dataframe to the same
+# filename as the dataset
+
+
+# SAVE OUTPUTS
+
+IDS_TO_REMOVE = pd.DataFrame(columns=["record_ids"])
+
+# DUMMY_CONTAINER_OUTPUT_PATHS is a single path to a directory
+results_dir = os.environ["DUMMY_CONTAINER_OUTPUT_PATHS"]
+
+output_path = f"{results_dir}{os.path.basename(dataset_path)}.parquet"
+logging.info(f"Writing output for dataset from input {dataset_path} to {output_path}")
+IDS_TO_REMOVE.to_parquet(output_path)
