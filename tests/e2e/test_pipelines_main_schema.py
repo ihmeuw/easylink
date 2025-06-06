6a6aa299b385d8ce4c58d74f7492139aef6809d0
Zeb Burke-Conte <zmbc@uw.edu>
2025-06-06 15:11:39 -0700

    Merge tests

--- tests/e2e/test_pipelines_main_schema.py
+++ tests/e2e/test_pipelines_main_schema.py
@@ -115,9 +115,23 @@ def test_pipeline_splink_dummy(
             "tests/specifications/common/environment_local.yaml",
             "tests/e2e/pipeline_splink_dummy_results.csv",
         ),
+        # slurm pipeline_cascade.yaml
+        (
+            "tests/specifications/e2e/pipeline_cascade.yaml",
+            "tests/specifications/e2e/input_data_dummy.yaml",
+            "tests/specifications/e2e/environment_slurm.yaml",
+            "tests/e2e/pipeline_splink_dummy_results.csv",
+        ),
+        # local pipeline_cascade.yaml
+        (
+            "tests/specifications/e2e/pipeline_cascade.yaml",
+            "tests/specifications/e2e/input_data_dummy.yaml",
+            "tests/specifications/common/environment_local.yaml",
+            "tests/e2e/pipeline_splink_dummy_results.csv",
+        ),
     ],
 )
-def test_pipeline_with_fastLink(
+def test_pipelines_same_output_relabeled(
     pipeline_specification,
     input_data,
     computing_environment,
@@ -170,97 +184,6 @@ def test_pipeline_with_fastLink(
         # However, the clusters won't be *labeled* with the same names.
         # This overly-tricky bit of code checks that the actual clusters induced are the same,
         # whether or not they are labeled the same.
-        assert frozenset(
-            results.groupby("Cluster ID")["Input Record ID"].apply(frozenset)
-        ) == frozenset(
-            splink_results.groupby("Cluster ID")["Input Record ID"].apply(frozenset)
-        )
-
-        assert (test_specific_results_dir / Path(pipeline_specification).name).exists()
-        assert (test_specific_results_dir / Path(input_data).name).exists()
-        assert (test_specific_results_dir / Path(computing_environment).name).exists()
-
-        print(
-            "\n\n*** END OF TEST ***\n"
-            f"[{pipeline_specification}, {input_data}, {computing_environment}]\n"
-        )
-
-@pytest.mark.slow
-@pytest.mark.skipif(
-    not is_on_slurm(),
-    reason="Must be on slurm to run this test.",
-)
-@pytest.mark.parametrize(
-    "pipeline_specification, input_data, computing_environment, splink_results_csv",
-    [
-        # slurm pipeline_with_fastLink.yaml
-        (
-            "tests/specifications/e2e/pipeline_cascade.yaml",
-            "tests/specifications/e2e/input_data_dummy.yaml",
-            "tests/specifications/e2e/environment_slurm.yaml",
-            "tests/e2e/pipeline_splink_dummy_results.csv",
-        ),
-        # local pipeline_with_fastLink.yaml
-        (
-            "tests/specifications/e2e/pipeline_cascade.yaml",
-            "tests/specifications/e2e/input_data_dummy.yaml",
-            "tests/specifications/common/environment_local.yaml",
-            "tests/e2e/pipeline_splink_dummy_results.csv",
-        ),
-    ],
-)
-def test_pipeline_cascade(
-    pipeline_specification,
-    input_data,
-    computing_environment,
-    splink_results_csv,
-    test_specific_results_dir,
-    capsys,
-):
-    """Tests the 'easylink run' command
-
-    Notes
-    -----
-    We use various print statements in this test because they show up in the
-    Jenkins logs.
-    """
-
-    with capsys.disabled():  # disabled so we can monitor job submissions
-        print(
-            "\n\n*** RUNNING TEST ***\n"
-            f"[{pipeline_specification}, {input_data}, {computing_environment}]\n"
-        )
-
-        cmd = (
-            "easylink run "
-            f"-p {pipeline_specification} "
-            f"-i {input_data} "
-            f"-e {computing_environment} "
-            f"-o {str(test_specific_results_dir)} "
-            "--no-timestamp "
-            "--schema main "
-        )
-        subprocess.run(
-            cmd,
-            shell=True,
-            stdout=sys.stdout,
-            stderr=sys.stderr,
-            check=True,
-        )
-        final_output = test_specific_results_dir / "result.parquet"
-        assert final_output.exists()
-        # Check that the results file matches the expected value
-        results = (
-            pd.read_parquet(final_output)
-            .sort_values("Input Record ID")
-            .reset_index(drop=True)
-        )
-        splink_results = pd.read_csv(splink_results_csv)
-        # TODO: Cascading hasn't actually found any additional records here,
-        # because they are currently all so easy to link that the first model
-        # gets them all.
-        # We should make our dummy files a bit harder.
-        # Tricky code from fastLink test.
         assert frozenset(
             results.groupby("Cluster ID")["Input Record ID"].apply(frozenset)
         ) == frozenset(
